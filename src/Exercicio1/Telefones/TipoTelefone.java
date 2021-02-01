35d2e8a7629591520574a5651c8f3036aea08aa0
Gabriel Martinello <martinellogabriel@gmail.com>
2021-02-01 15:00:53 -0200

    Exercicio 1

---
+++ src/Exercicio1/Telefones/TipoTelefone.java
@@ -0,0 +1,23 @@
+package Exercicio1.Telefones;
+
+public enum TipoTelefone {
+    COMERCIAL("Comercial"),
+    RESIDENCIAL("Residencial"),
+    CELULAR("Celular");
+
+    private String descricao;
+
+    TipoTelefone(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoTelefone" +
+                "/ndescricao = " + descricao;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import Exercicio1.Endereco.Endereco;
+import Exercicio1.Pessoa.Pessoa;
+import Exercicio1.Telefones.Telefone;
+import Exercicio1.Telefones.TipoTelefone;
+
+import java.util.Arrays;
+
 public class Main {
 //Exercicio 1
 
     public static void main(String[] args) {
-
-
-
+        Pessoa pessoa = new Pessoa("Joao",
+                "Pessoa",
+                "090.497.639-44",
+                "Gabriel@gmail.com",
+                "07/08/2004",
+                Arrays.asList(new Telefone("12233344", TipoTelefone.CELULAR)),
+                Arrays.asList(
+                        new Endereco("Francisco Brochado da Rocha",
+                                309,
+                                "Industrial")
+                )
+        );
+        System.out.println("------------------------------------------------");
+        System.out.println(pessoa.toString());
+        System.out.println("------------------------------------------------");
     }
 
 }
+
+
