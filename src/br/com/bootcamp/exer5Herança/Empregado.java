d1e495787299151b976456cd1cab2038faa9a14a
Gabriel Martinello <martinellogabriel@gmail.com>
2021-02-02 18:23:10 -0200

    Exercicio 5

---
+++ src/br/com/bootcamp/exer5Herança/Empregado.java
@@ -0,0 +1,38 @@
+package br.com.bootcamp.exer5Herança;
+
+public class Empregado extends Pessoa {
+
+    private double valorSalarioBase;
+    private double valorImpostos;
+
+    public Empregado(String nome,
+                     String cpf,
+                     double valorSalarioBase,
+                     double valorImpostos) {
+        /* chama o construtor da classe pessoa */
+        super(nome, cpf);
+        this.valorImpostos = valorImpostos;
+        this.valorSalarioBase = valorSalarioBase;
+    }
+
+    public double calculaValorTotalDoSalario() {
+        return valorSalarioBase - valorImpostos;
+    }
+
+    public double getValorImpostos() {
+        return valorImpostos;
+    }
+
+    public String mostrarNome() {
+        return this.getNome();
+    }
+
+    public String mostrarCpf() {
+        return this.getCpf();
+    }
+
+    public double getValorSalarioBase() {
+        return valorSalarioBase;
+    }
+
+}
